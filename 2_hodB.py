67cfded43ca21b7080bb05d034c78a04b58f3759
Milda2000 <58146822+Milda2000@users.noreply.github.com>
2021-11-01 21:46:11 +0100

    Delete 2_hodB.py

--- 2_hodB.py
+++
@@ -1,8 +0,0 @@
-deg_float = float(input("zadejte souřadnice jako desetinné číslo: "))
-deg = int(deg_float)
-min_float = (deg_float - deg)*60 
-min = int(min_float)
-sec = (min_float - min)*60
-
-print(f"{deg} stupňů {min} minut {round(sec)} sekund")
-
